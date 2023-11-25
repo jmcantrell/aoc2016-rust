ad487ed780b7cf7f8161b859b5a683ac2bfed2a9
Jeremy Cantrell <jmcantrell@gmail.com>
2023-11-25 15:06:44 -0600

    Add solution for day 4

---
+++ puzzles/day04/src/answer/solve.rs
@@ -0,0 +1,39 @@
+use anyhow::Context;
+
+use super::{Parsed1, Parsed2};
+
+type Solution = usize;
+pub type Solution1 = Solution;
+pub type Solution2 = Solution;
+
+pub fn solve1(room_codes: &Parsed1) -> anyhow::Result<Solution1> {
+    Ok(room_codes
+        .iter()
+        .filter(|code| code.is_real())
+        .map(|code| code.sector_id)
+        .sum())
+}
+
+pub fn solve2(room_codes: &Parsed2) -> anyhow::Result<Solution2> {
+    room_codes
+        .iter()
+        .find_map(|code| {
+            (code.decrypt_name() == "northpole object storage").then_some(code.sector_id)
+        })
+        .context("no solution")
+}
+
+#[cfg(test)]
+mod tests {
+    use aoc::Input;
+
+    use crate::answer::parse1;
+
+    const INPUT: Input = include_str!("../../input-test.txt");
+
+    #[test]
+    fn solve1() -> anyhow::Result<()> {
+        assert_eq!(super::solve1(&parse1(INPUT)?)?, 1514);
+        Ok(())
+    }
+}
---
+++ puzzles/day04/src/core/code.rs
@@ -0,0 +1,116 @@
+use anyhow::Context;
+use lazy_regex::{lazy_regex, Lazy, Regex};
+
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashMap};
+use std::convert::TryFrom;
+
+static RE: Lazy<Regex> =
+    lazy_regex!(r"^(?<name_encrypted>[a-z-]+)-(?<sector_id>\d+)\[(?<checksum>[a-z]+)\]$");
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct RoomCode<'a> {
+    pub name_encrypted: &'a str,
+    pub sector_id: usize,
+    pub checksum: &'a str,
+}
+
+impl RoomCode<'_> {
+    pub fn is_real(&self) -> bool {
+        let counts: HashMap<char, usize> =
+            self.name_encrypted
+                .chars()
+                .filter(|&c| c != '-')
+                .fold(HashMap::new(), |mut acc, c| {
+                    *acc.entry(c).or_default() += 1;
+                    acc
+                });
+
+        let mut freq: BinaryHeap<(usize, Reverse<char>)> =
+            counts.into_iter().map(|(c, n)| (n, Reverse(c))).collect();
+
+        let mut checksum = String::new();
+
+        for _ in 0..self.checksum.len() {
+            checksum.push(freq.pop().unwrap().1 .0);
+        }
+
+        checksum == self.checksum
+    }
+
+    pub fn decrypt_name(&self) -> String {
+        let n = 26u8;
+        let offset = 97u8;
+        let shift = (self.sector_id % n as usize) as u8;
+        let mut name = String::new();
+
+        for c in self.name_encrypted.chars() {
+            if c == '-' {
+                name.push(' ');
+            } else {
+                let i = (c as u8) - offset;
+                let j = (i + shift) % n + offset;
+                name.push(j as char);
+            }
+        }
+
+        name
+    }
+}
+
+impl<'a> TryFrom<&'a str> for RoomCode<'a> {
+    type Error = anyhow::Error;
+
+    fn try_from(s: &'a str) -> anyhow::Result<Self> {
+        let captures = RE
+            .captures(s)
+            .with_context(|| format!("expected input to match: {:?}", RE.as_str()))?;
+
+        let name_encrypted = captures.name("name_encrypted").unwrap().as_str();
+        let sector_id = captures["sector_id"].parse()?;
+        let checksum = captures.name("checksum").unwrap().as_str();
+
+        Ok(Self {
+            name_encrypted,
+            sector_id,
+            checksum,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn is_real() {
+        macro_rules! assert_real {
+            ($input:expr) => {
+                assert!(super::RoomCode::try_from($input).unwrap().is_real());
+            };
+        }
+        macro_rules! assert_not_real {
+            ($input:expr) => {
+                assert!(!super::RoomCode::try_from($input).unwrap().is_real());
+            };
+        }
+
+        assert_real!("aaaaa-bbb-z-y-x-123[abxyz]");
+        assert_real!("a-b-c-d-e-f-g-h-987[abcde]");
+        assert_real!("not-a-real-room-404[oarel]");
+
+        assert_not_real!("totally-real-room-200[decoy]");
+    }
+
+    #[test]
+    fn decrypt_name() {
+        macro_rules! test {
+            ($input:expr => $expected:expr) => {
+                assert_eq!(
+                    super::RoomCode::try_from($input).unwrap().decrypt_name(),
+                    $expected
+                );
+            };
+        }
+
+        test!("qzmt-zixmtkozy-ivhz-343[xxxxx]" => "very encrypted name");
+    }
+}
